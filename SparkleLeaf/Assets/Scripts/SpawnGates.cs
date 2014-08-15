dab40d2157028c9a2632610b4d95370a1a97d0cc
InFinitySqrd <jFinlayson94@gmail.com>
2014-08-15 15:07:05 +1000

    Added the ability to spawn different obstacles

--- SparkleLeaf/Assets/Scripts/SpawnGates.cs
+++ SparkleLeaf/Assets/Scripts/SpawnGates.cs
@@ -4,13 +4,16 @@ using System.Collections.Generic;
 
 public class SpawnGates : MonoBehaviour {
 	[SerializeField] int numRotations = 10;
-	[SerializeField] Transform gate;
 	[SerializeField] float spawnTime = 6.0f;
 	[SerializeField] float spawnDistance = 10.0f;
 	[SerializeField] float closeRotation = 5.0f;
 	[SerializeField] float colourChangeRate = 4.0f;
+	
+	[SerializeField] Transform[] obstacles;
+	[SerializeField] float[] percentageSpawnChance; 
+	
 	private float timer = 0.0f;
-	private Transform spawnedGate;
+	private Transform spawnedObstacle;
 	private List<Transform> gatesList;
 	private int score = 0;
 	private Color planeColor;
@@ -25,6 +28,19 @@ public class SpawnGates : MonoBehaviour {
 		timer = spawnTime;
 		planeColor = this.renderer.material.color;
 		planeBodyColor = this.transform.GetChild(0).renderer.material;
+		
+		if (obstacles.Length != percentageSpawnChance.Length) {
+			Debug.LogError("Not an equal number of objects and percentages");
+		}
+		
+		float percentage = 0.0f;
+		for (int i = 0; i < percentageSpawnChance.Length; i++) {
+			percentage += percentageSpawnChance[i];
+		}
+		
+		if (percentage != 1.0f) {
+			Debug.LogError("Percentages do not add up to 1.0");
+		}
 	}
 	
 	void OnGUI() {
@@ -43,12 +59,22 @@ public class SpawnGates : MonoBehaviour {
 	
 		if (timer >= spawnTime) {
 			Vector3 spawnPoint = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + spawnDistance);
-			spawnedGate = (Transform)Instantiate(gate, spawnPoint, Quaternion.identity);
 			
-			gatesList.Add(spawnedGate);
+			float randomNumber = Random.value;
+			float summedProbabilities = 0.0f;
+			
+			for (int i = 0; i < obstacles.Length; i++) {
+				summedProbabilities += percentageSpawnChance[i];
+				
+				if (randomNumber < summedProbabilities) {
+					spawnedObstacle = (Transform)Instantiate(obstacles[i], spawnPoint, Quaternion.identity);
+				}
+			}
+			
+			gatesList.Add(spawnedObstacle);
 			
 			int randomNotch = (int)Random.Range(0, numRotations);
-			spawnedGate.transform.eulerAngles = new Vector3(spawnedGate.transform.eulerAngles.x, spawnedGate.transform.eulerAngles.y, randomNotch * (360.0f / numRotations));
+			spawnedObstacle.transform.eulerAngles = new Vector3(spawnedObstacle.transform.eulerAngles.x, spawnedObstacle.transform.eulerAngles.y, randomNotch * (360.0f / numRotations));
 			
 			timer = 0.0f;
 		} else {
