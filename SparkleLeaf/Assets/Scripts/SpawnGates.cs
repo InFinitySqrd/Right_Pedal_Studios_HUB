b42bf1b4cea13e727716b9f83a34db4c7f85ad77
InFinitySqrd <jFinlayson94@gmail.com>
2014-08-15 11:24:33 +1000

    Colouring based off of rotation difference enabled

--- SparkleLeaf/Assets/Scripts/SpawnGates.cs
+++ SparkleLeaf/Assets/Scripts/SpawnGates.cs
@@ -7,10 +7,14 @@ public class SpawnGates : MonoBehaviour {
 	[SerializeField] Transform gate;
 	[SerializeField] float spawnTime = 6.0f;
 	[SerializeField] float spawnDistance = 10.0f;
+	[SerializeField] float closeRotation = 5.0f;
+	[SerializeField] float colourChangeRate = 4.0f;
 	private float timer = 0.0f;
 	private Transform spawnedGate;
 	private List<Transform> gatesList;
 	private int score = 0;
+	private Color planeColor;
+	private Material planeBodyColor;
 	
 	void Awake() {
 		gatesList = new List<Transform>();
@@ -19,6 +23,8 @@ public class SpawnGates : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		timer = spawnTime;
+		planeColor = this.renderer.material.color;
+		planeBodyColor = this.transform.GetChild(0).renderer.material;
 	}
 	
 	void OnGUI() {
@@ -31,6 +37,10 @@ public class SpawnGates : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (gatesList.Count > 0) {
+			RotationColour();
+		}
+	
 		if (timer >= spawnTime) {
 			Vector3 spawnPoint = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + spawnDistance);
 			spawnedGate = (Transform)Instantiate(gate, spawnPoint, Quaternion.identity);
@@ -48,6 +58,17 @@ public class SpawnGates : MonoBehaviour {
 		if (this.transform.position.z > gatesList[0].position.z) {
 			score++;
 			gatesList.RemoveAt(0);
+		}
+	}
+	
+	private void RotationColour() {
+		if (Quaternion.Angle(this.transform.rotation, gatesList[0].transform.rotation) <= closeRotation ||
+		    Quaternion.Angle(this.transform.rotation, gatesList[0].transform.rotation) >= 180.0f - closeRotation) {
+			this.renderer.material.color = Color.Lerp(this.renderer.material.color, Color.green, Time.deltaTime * colourChangeRate);
+			planeBodyColor.color = this.renderer.material.color;
+		} else {
+			this.renderer.material.color = Color.Lerp(this.renderer.material.color, planeColor, Time.deltaTime * colourChangeRate * 4.0f);
+			planeBodyColor.color = this.renderer.material.color;
 		}
 	}
 }
