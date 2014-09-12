215f341355b58086e2d39c16dd43f6222c90d15d
InFinitySqrd <jFinlayson94@gmail.com>
2014-09-12 11:39:28 +1000

    Added monster spawn animation & splash

---
+++ SparkleLeaf/Assets/Scripts/Interface/SetToScreenSize.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetToScreenSize : MonoBehaviour {
+	// Declare variables
+	[SerializeField] bool mainMenu = false;
+
+	// Use this for initialization
+	void Start () {
+		if (!mainMenu) {
+			this.transform.guiTexture.pixelInset = new Rect(-Screen.width / 2, -Screen.height/2, Screen.width, Screen.height);
+		} else {
+			this.transform.guiTexture.pixelInset = new Rect(-Screen.width * 1.1f / 2, -Screen.height * 1.2f / 2, Screen.width * 1.1f, Screen.height * 1.2f);
+		}
+
+
+
+		//this.transform.guiTexture.pixelInset.y = Screen.height / 2;
+		//this.transform.guiTexture.pixelInset.width = Screen.width;
+		//this.transform.guiTexture.pixelInset.height = Screen.height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- SparkleLeaf/Assets/Scripts/Interface/SetUpCredits.cs
+++ SparkleLeaf/Assets/Scripts/Interface/SetUpCredits.cs
@@ -3,20 +3,39 @@ using System.Collections;
 
 public class SetUpCredits : MonoBehaviour {
     // Declare variables
-    [SerializeField] Transform title, creditsList, backButton;
+    [SerializeField] Transform creditsList, backButton;
+	[SerializeField] Font fontStyle;
+
+	private float fadeSpeed = 0.0f;
+	private Color textColour;
 
 	void Awake () {
 	    // Set up all UI elements to scale with screen size
-        title.transform.position = this.camera.ScreenToWorldPoint(new Vector3(Screen.width / 2.0f, 3.0f * Screen.height / 4.0f, 1.0f));
-    }
+		creditsList.transform.position = this.camera.ScreenToWorldPoint(new Vector3(Screen.width / 2.0f, Screen.height / 2.2f, 1.0f));
+		backButton.transform.position = this.camera.ScreenToWorldPoint(new Vector3(0.0f + Screen.width / 8.0f, Screen.height - Screen.height / 12.0f, 1.0f));
+    
+		fadeSpeed = this.transform.GetChild(0).GetComponent<MenuTween>().fadeSpeed;
+		textColour = new Color(Color.white.r, Color.white.g, Color.white.b, 0.0f);
+	}
 
-    // Use this for initialization
-    void Start() {
+	void Start() {
+		StartCoroutine("FadeTextIn");
+	}
 
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnGUI() {
+		GUIStyle skin = new GUIStyle();
+		skin.font = fontStyle;
+		skin.fontSize = 36;
+		skin.normal.textColor = textColour;
+		skin.alignment = TextAnchor.MiddleCenter;
+
+		GUI.Box(new Rect(0.0f, 0.0f, Screen.width, Screen.height), "\nDebra Polson\nNathaniel Holloway\nJames Finlayson\nWade Taylor\nRachel Grieveson\nNathan Corporal\n Nicky Watson", skin);
+	}
+
+	IEnumerator FadeTextIn() {
+		while (textColour.a <= 1.0f) {
+			textColour = new Color(Color.white.r, Color.white.g, Color.white.b, textColour.a + Time.deltaTime * fadeSpeed);
+			yield return null;
+		}
 	}
 }
