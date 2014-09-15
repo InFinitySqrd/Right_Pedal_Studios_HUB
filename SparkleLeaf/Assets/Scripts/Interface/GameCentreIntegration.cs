ed875a281c6b1c34f6607abda59a4a7df43d80fc
InFinitySqrd <jFinlayson94@gmail.com>
2014-09-15 18:50:47 +1000

    Added Early Leaderboard Integration
    Moved Plane Model into appropriate position

---
+++ SparkleLeaf/Assets/Scripts/Interface/GameCentreIntegration.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.SocialPlatforms.GameCenter;
+using System.Collections;
+
+public class GameCentreIntegration : MonoBehaviour {
+    // Declare variables
+    ILeaderboard leaderboard;
+
+    void Start() {
+        // Register the user
+        Social.localUser.Authenticate(ProcessAuthentication);
+        leaderboard = Social.CreateLeaderboard();
+        leaderboard.id = "scores";
+    }
+
+    private void ProcessAuthentication(bool success) {
+        if (success) {
+            Debug.Log("Authenticated");
+        } else {
+            Debug.Log("Failed to Authenticate");
+        }
+    }
+
+    public void GetLeaderboardResults() {
+        leaderboard.LoadScores(result => ReadLeaderboard(result));
+    }
+
+    private void ReadLeaderboard(bool result) {
+        if (leaderboard.scores.Length > 0) {
+            Debug.Log("Loaded " + leaderboard.scores.Length + " scores");
+
+            string myScores = "Leaderboard:\n";
+
+            foreach (IScore score in leaderboard.scores) {
+                myScores += "\t" + score.userID + " " + score.formattedValue + " " + score.date + "\n";
+            }
+
+            Debug.Log(myScores);
+        } else {
+            Debug.Log("No scores loaded");
+        }
+    }
+
+    public void WriteLeaderboard(long scoreValue) {
+        Debug.Log("Reporting score " + scoreValue + " to leaderboard " + leaderboard.id);
+
+        Social.ReportScore(scoreValue, leaderboard.id, success => {
+            Debug.Log(success ? "Success":"Failed");
+        });
+    }
+
+    public void DisplayDefaultLeaderboard() {
+        Social.ShowLeaderboardUI();
+    }
+}
