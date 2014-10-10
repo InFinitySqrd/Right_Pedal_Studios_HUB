bbdac0935a0d4221123636f1f12f20322edaeec9
InFinitySqrd <jFinlayson94@gmail.com>
2014-10-10 12:45:53 +1000

    Changes to the moon and art fixes

--- SparkleLeaf/Assets/Scripts/Visual Scripts/MoonMovement.cs
+++ SparkleLeaf/Assets/Scripts/Visual Scripts/MoonMovement.cs
@@ -4,39 +4,46 @@ using System.Collections;
 public class MoonMovement : MonoBehaviour {
     // Declare variables
     [SerializeField] float moveSpeed = 0.0001f;
+    [SerializeField] float moonDuration = 20.0f;
     [SerializeField] float timeToNewMoon = 10.0f;
+    [SerializeField] float timeBetweenXAdditions = 4.0f;
     private Vector3 movementVector;
     private Vector3 initialSpawnPos;
+    private Rect initialSpawnRect;
 
 	// Use this for initialization
 	void Start () {
         // Set the initial position of the moon
-	    //this.guiTexture.pixelInset = new Rect(0.0f - Screen.width / 8.0f, 0.0f - Screen.height / 3.0f, Screen.width / 4.0f, Screen.width / 4.0f);
-        initialSpawnPos = new Vector3(0.0f, -200.0f, 400.0f);
-        this.transform.position = initialSpawnPos;
-        this.transform.localScale = new Vector3(20.0f, 20.0f, 20.0f);
+        initialSpawnPos = this.transform.position;
+        initialSpawnRect = new Rect(0.0f - Screen.width / 8.0f, 0.0f - Screen.height / 3.0f, Screen.width / 4.0f, Screen.width / 4.0f);
+	    this.guiTexture.pixelInset = initialSpawnRect;
+
+        //initialSpawnPos = new Vector3(0.0f, -200.0f, 400.0f);
+        //this.transform.position = initialSpawnPos;
+        //this.transform.localScale = new Vector3(20.0f, 20.0f, 20.0f);
 
         // Initialise the movement of the moon
         movementVector = Vector3.up;
 
         // Move the moon in a slight Y direction
-        //StartCoroutine(SetYValue());
+        StartCoroutine(SetXValue());
+        StartCoroutine(MoonCounter());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    this.transform.position = Vector3.MoveTowards(this.transform.position, this.transform.position + movementVector, Time.deltaTime * moveSpeed);
-
-        if (!this.renderer.isVisible && this.transform.position.y > 0.0f) {
-            // When the moon has left the screen, reassign it's texture and reset it's position
-            StartCoroutine(ResetMoonPosition());
-        }
     }
 
-    IEnumerator SetYValue() {
+    IEnumerator MoonCounter() {
+        yield return new WaitForSeconds(moonDuration);
+        StartCoroutine(ResetMoonPosition());
+    }
+
+    IEnumerator SetXValue() {
         movementVector = new Vector3(movementVector.x + moveSpeed, movementVector.y, movementVector.z);
-        yield return new WaitForSeconds(4.0f);
-        StartCoroutine(SetYValue());
+        yield return new WaitForSeconds(timeBetweenXAdditions);
+        StartCoroutine(SetXValue());
     }
 
     IEnumerator ResetMoonPosition() {
@@ -44,7 +51,11 @@ public class MoonMovement : MonoBehaviour {
 
         // Reset position
         this.transform.position = initialSpawnPos;
-
+        movementVector = Vector3.up;
+        
         // Reassign a different coloured texture;
+
+        // Start the moon moving again
+        StartCoroutine(MoonCounter());
     }
 }
