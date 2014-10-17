65505a38498834b57262e79845eb9ea8e7a8fefc
NCorporal <n8550646@cif-fmd-mini01.qut.edu.au>
2014-10-17 18:17:12 +1000

    Chartboost

---
+++ SparkleLeaf/Assets/Scripts/Interface/ChartboostAds.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using ChartboostSDK;
+
+public class ChartboostAds : MonoBehaviour {
+
+	void Awake() {
+		CBExternal.init ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		CBExternal.showInterstitial (CBLocation.Default);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
